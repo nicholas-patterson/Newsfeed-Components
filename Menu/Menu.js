3bd7886ae4f43449816a1341b56ece692869c466
Nicholas Patterson <nicholaspatterson@Nicholass-MBP.home>
2019-06-19 18:49:33 -0400

    Tried the stretch problem

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,7 @@
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle("menu--open");
-};
+//const toggleMenu = () => {
+// Toggle the "menu--open" class on your menu refence.
+//menu.classList.toggle("menu--open");
+//};
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
@@ -9,4 +9,19 @@ const menu = document.querySelector(".menu");
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
 
-menuButton.addEventListener("click", () => toggleMenu());
+//menuButton.addEventListener("click", () => toggleMenu());
+
+// JQUERY STRETCH
+
+$(".menu-button").click(function() {
+  $(".menu").animate(
+    {
+      right: "-=50",
+      height: "toggle",
+      direction: "left"
+      //left: 1000
+    },
+    2000,
+    function() {}
+  );
+});
